df3176b3fb3b47a542aa6cc32c450ac18005e937
renchan <1044123085@qq.com>
2022-05-22 22:22:47 +0800

    新增user接口中的与关注有关的三个接口实现 新增pack下将db.User转换为user.User的函数

---
+++ cmd/user/pack/user.go
@@ -0,0 +1,17 @@
+package pack
+
+import (
+	"douyin-micro/cmd/user/dal/db"
+	"douyin-micro/kitex_gen/user"
+)
+
+//ConvUser 将模型的user 转换为 响应里的user
+func ConvUser(prev *db.User) *user.User {
+	result := &user.User{
+		Id:            prev.ID,
+		Name:          prev.Name,
+		FollowCount:   &prev.FollowerCount,
+		FollowerCount: &prev.FollowerCount,
+		IsFollow:      prev.IsFollow}
+	return result
+}
