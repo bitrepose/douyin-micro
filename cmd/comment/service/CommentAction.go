eac8512ea3287492592a9b5e9cfc9e4fe73898a5
Baigeiguai <1927405047@stu.suda.edu.cn>
2022-05-25 14:29:32 +0800

    1

--- cmd/comment/service/CommentAction.go
+++ cmd/comment/service/CommentAction.go
@@ -3,8 +3,13 @@ package service
 import (
 	"context"
 	"douyin-micro/cmd/comment/dal/db"
+	"douyin-micro/pkg/constants"
 )
 
-func PostComment(ctx context.Context, cmt *db.Comment)error{
-	
+func PostComment(ctx context.Context, cmt *db.Comment) (*db.Comment,constants.PostCommentMessage) {
+		
+}
+
+func DeleteComent(ctx context.Context,cmt *db.Comment)constants.DeleteCommentMessage{
+
 }
