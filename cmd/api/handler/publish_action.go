149133e4d73b91ca994c192fa17343c51fd504f3
eamon <neo926@163.com>
2022-05-27 20:33:28 +0800

    增加base_resp

--- cmd/api/handler/publish_action.go
+++ cmd/api/handler/publish_action.go
@@ -1,7 +1,15 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"douyin-micro/pkg/errno"
+
+	"github.com/gin-gonic/gin"
+)
 
 func PublishAction(c *gin.Context) {
-	
+	file, err := c.FormFile("data")
+	if err != nil {
+		sendBaseResp(c, errno.ConvertErr(err))
+	}
+	// req := video.
 }
