2c2e512de22d5d3f21cb17fe7dd6c243af7ba0c5
Spidey0918 <445906160@qq.com>
2022-05-28 17:06:38 +0800

    api部分comment实现，部分optional param还没处理

--- cmd/api/rpc/comment.go
+++ cmd/api/rpc/comment.go
@@ -1,8 +1,11 @@
 package rpc
 
 import (
+	"context"
+	"douyin-micro/kitex_gen/comment"
 	"douyin-micro/kitex_gen/comment/commentservice"
 	"douyin-micro/pkg/constants"
+	"douyin-micro/pkg/errno"
 	"douyin-micro/pkg/middleware"
 	"time"
 
@@ -35,4 +38,26 @@ func initCommentRpc() {
 		panic(err)
 	}
 	commentClient = c
+}
+
+func CommentList(ctx context.Context, req *comment.CommentListRequest) (*comment.CommentListResponse, error) {
+	resp, err := commentClient.CommentList(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != errno.SuccessCode {
+		return nil, errno.NewErrNo(int64(resp.StatusCode), *resp.StatusMsg)
+	}
+	return resp, nil
+}
+
+func CommentAction(ctx context.Context, req *comment.CommentActionRequest) error {
+	resp, err := commentClient.CommentAction(ctx, req)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != errno.SuccessCode {
+		return errno.NewErrNo(int64(resp.StatusCode), *resp.StatusMsg)
+	}
+	return nil
 }
